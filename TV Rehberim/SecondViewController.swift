c79b49dfd81eb9a6f0e7d80363768ab846dddb10
Nevzat BOZKURT <nevzatbozkurt@nevzat-macbook-pro.local>
2019-01-06 19:42:27 +0300

    Initial Commit

---
+++ TV Rehberim/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TV Rehberim
+//
+//  Created by Nevzat BOZKURT on 6.01.2019.
+//  Copyright © 2019 Nevzat BOZKURT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
